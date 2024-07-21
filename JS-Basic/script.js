18163091793a68e828ab396f667bba1678900edc
Nillotpal Mishra <nillmishra09@gmail.com>
2024-07-21 23:50:44 +0530

    basic of javascript

---
+++ JS-Basic/script.js
@@ -0,0 +1,46 @@
+// console.log('Chailiye shru karte hai');
+// let rectangle = {
+//     length: 1,
+//     breadth: 2,
+//     draw: function()  {
+//         console.log("draw");
+//     }
+// };
+
+// //factory function
+
+
+// function createRectangle(len, bre){
+//     let rectangle = {
+//         length: len,
+//         breadth: bre,
+//         draw: function()  {
+//             console.log("draw");
+//         }
+//     };
+//     return rectangle;
+// }
+
+// let rectangleObj1 = createRectangle(4, 5);
+// let rectangleObj2 = createRectangle(4, 5);
+// let rectangleObj3 = createRectangle(4, 5);
+
+
+// constructor function
+
+// function Rectangle(len, bre){
+//     this.length = len;
+//     this.breadth = bre;
+//     this.draw = function(){
+//         console.log('drawing');
+//     }
+// }
+
+// let rectangleObject = new Rectangle(4, 6);
+
+// rectangleObject.color = 'yellow';  
+// console.log(rectangleObject);
+
+// delete rectangleObject.color;
+// console.log(rectangleObject)
+
---
+++ JS-Basic/string.js
@@ -0,0 +1,19 @@
+console.log("Hello Ji")
+
+let lastName = "Mishra";
+
+let firstName = new String('Nill');
+
+
+/* 
+typeof lastName
+'string'
+typeof firstName
+'object'
+*/
+
+let message = "This is my first message";
+
+let words = message.split(' ')
+
+console.log(words)
