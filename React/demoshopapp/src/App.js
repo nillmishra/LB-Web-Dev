2632dcc33a3046dbe1ec81f162d5f58d4ea0277c
Nillotpal Mishra <nillmishra09@gmail.com>
2024-08-11 12:32:20 +0530

    some change file removed

--- React/demoshopapp/src/App.js
+++ React/demoshopapp/src/App.js
@@ -1,40 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
-import Item from './component/Item';  
-import ItemDate from './component/ItemDate';
-import Card from './component/card'
+import React from 'react';
+import NewProduct from './components/NewProduct';
 
-function App() {
-  const products = [{
-    id : 'p1',
-    title : 'Surf Excel',
-    price : '130',
-    date : new Date(2021, 7, 10)
-  },
-  {
-    id : 'p2',
-    title : 'Ariel',
-    price : '135',
-    date : new Date(2021, 8, 10)
-  },
-  {
-    id : 'p3',
-    title : 'Tide',
-    price : '140',
-    date : new Date(2021, 2, 2)
-  },
-  {
-    id : 'p4',
-    title : 'Nirma',
-    price : '120',
-    date : new Date(2021, 12, 28)
+import Products from './components/Products';
+
+const App = () => {
+  const products = [
+    {
+      id: 'p1',
+      title: 'Nirma',
+      amount: 100,
+      date: new Date(2021, 8, 10),
+    },
+    { 
+      id: 'p2', 
+      title: 'Sirf Excel', 
+      amount: 200, 
+      date: new Date(2021, 2, 2) },
+    {
+      id: 'p3',
+      title: 'Tide',
+      amount: 130,
+      date: new Date(2021, 12, 28),
+    },
+    {
+      id: 'p4',
+      title: 'Maggi',
+      amount: 450,
+      date: new Date(2021, 5, 5),
+    },
+  ];
+
+  function printProductData(data) {
+    console.log("i am inside APP.js")
+    console.log(data)
   }
-]
 
   return (
-    
     <div>
-    <Products items={products} />
+      <NewProduct pranay = {printProductData} />
+      <Products items={products} />
     </div>
   );
 }
